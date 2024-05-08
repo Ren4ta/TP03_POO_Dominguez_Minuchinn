ad7503484349b132649af2e356b5c2adc5c1cfa3
Ren4ta <48313480@est.ort.edu.ar>
2024-05-08 08:26:22 -0300

    Creación de clase Tiquetera

--- Tiquetera.cs
+++ Tiquetera.cs
@@ -1,4 +1,26 @@
 static class Tiquetera 
 {
-    private Dictionary DicClientes<int,cliente>
+    private static int UltimoIDEntrada = 0; 
+
+    private static Dictionary<int, cliente> DicClientes = new Dictionary<int,cliente>();
+    public static int DevolverUltimoID() 
+    {
+        return; 
+    } 
+    public static int AgregarCliente(cliente Cliente) 
+    {
+
+    }
+    public static cliente BuscarCliente(int Id)
+    {
+
+    }
+    public static int CambiarEntrada(int Id, string Tipo, int Cantidad)
+    {
+
+    }
+    public static List<string> EstadisticasTicketera()
+    {
+
+    }
 }
